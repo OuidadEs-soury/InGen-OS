6541200f53d1664047effedcb88a6cbe4b62d391
Ouidad7 <widadelsouri@gmail.com>
2026-07-18 17:59:56 +0100

    animated terminal line component

--- client/src/components/system/BootSequence/BootSequence.tsx
+++ client/src/components/system/BootSequence/BootSequence.tsx
@@ -1,36 +1,57 @@
 import styles from "./BootSequence.module.css";
 
+import ProgressBar from "../ProgressBar/ProgressBar";
+import TerminalLine from "../TerminalLine/TerminalLine";
+
 import { useBootSequence } from "../../../hooks/useBootSequence";
 
-import ProgressBar from "../ProgressBar/ProgressBar";
-
 function BootSequence() {
-  const { messages, progress } = useBootSequence();
 
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.logo}>InGen OS</h1>
+    const {
+        messages,
+        progress,
+    } = useBootSequence();
 
-      <div className={styles.terminal}>
-        {messages.map((message, index) => (
-          <div
-            className={styles.row}
-            key={index}
-          >
-            <span className={styles.command}>
-              &gt; {message.text}
-            </span>
+    return (
 
-            <span className={styles.status}>
-              [{message.status}]
-            </span>
-          </div>
-        ))}
+        <div className={styles.container}>
 
-        <ProgressBar value={progress} />
-      </div>
-    </div>
-  );
+            <h1 className={styles.logo}>
+                InGen OS
+            </h1>
+
+            <div className={styles.terminal}>
+
+                <div className={styles.header}>
+
+                    <span></span>
+
+                    <span className={styles.title}>
+                        InGen BIOS v7.4.1
+                    </span>
+
+                    <span></span>
+
+                </div>
+
+                {messages.map((message,index)=>(
+
+                    <TerminalLine
+                        key={index}
+                        text={message.text}
+                        status={message.status}
+                    />
+
+                ))}
+
+                <ProgressBar value={progress}/>
+
+            </div>
+
+        </div>
+
+    );
+
 }
 
 export default BootSequence;
